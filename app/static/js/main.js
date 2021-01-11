8d4aeadb69bfee722e1891e37a593bb8702fba48
Silke Van Meerbeek <silkvanm1@student.arteveldehs.be>
2021-01-11 14:35:16 +0100

    veel js in orde gebracht

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -45,7 +45,7 @@ let studioModule = (function(){
 
     async function fetchstudio () {
         try {
-            const response = await fetch ('../app/data/atelier.json');
+            const response = await fetch ('../data/atelier.json');
             studioProjects = await response.json();
 
             printStudio();
@@ -78,11 +78,123 @@ let studioModule = (function(){
     }
 })();
 
+/*let categoryModule = (function () {
+    // const categorie = events.map(event => event.categorie);
+    // const filteredCategories = [...new Set(category)];
+    // console.log(filteredCategories);
+    const $filterCategories = document.querySelector('.filter-category');
+    let categorieFilter= [];
+
+    async function fetchCategories () {
+        try {
+            const response = await fetch (ART_API);
+            categorieFilter = await response.json();
+
+            printCategories();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function printCategories () {
+
+        $filterCategories.innerHTML = categorieFilter.map(artBig => {
+            return `
+            <li>${artBig.tags}</li>
+            `
+        }).join('');
+    }
+    function initialize () {
+        fetchCategories();
+    }
+
+    return {
+        initialize
+    }
+})();
+*/
+
+let artPageModule = (function () {
+    const $artPage = document.querySelector('.art-big');
+    let artPage = [];
+
+    async function fetchArtPage () {
+        try {
+            const response = await fetch (ART_API);
+            artPage = await response.json ();
+
+            printArtPage ();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function printArtPage () {
+        $artPage.innerHTML = artPage.map(artBig => {
+            return `
+            <li>
+                <h2>${artBig.title}</h2>
+                <h3>${artBig.subtitle}</h3>
+                <h4>${artBig.tags} — ${artBig.location}</h4>
+                <img class="art__photo" src="../static/images/${artBig.images}">
+            </li>
+            `
+        }).join('');
+    }
+    function initialize () {
+        fetchArtPage();
+    }
+
+    return {
+        initialize
+    }
+
+})();
+
+let pressModule =(function (){
+    const $pressList = document.querySelector('.container__press');
+    let pressList = [];
+
+    async function fetchPress () {
+        try {
+            const response = await fetch ('../data/press.json');
+            pressList = await response.json();
+
+            printPress();
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    function printPress () {
+        $pressList.innerHTML = pressList.map(projects => {
+            return `
+            <li class="press__list">
+                <img class="art__photo" src="../static/images/${projects.image}">
+                <h3>${projects.subtitle}</h3>
+                <h2 class="art__title">${projects.title}</h2>
+                <p>${projects.detail}</p>
+                <a class="learn-btn" href="#">Learn more</a>
+            </li>
+            `
+        }).join('');
+    }
+    function initialize () {
+        fetchPress();
+    }
+    return {
+        initialize
+    }
+})();
+
 let app = (function (){
 
     function initialize(){
         artModule.initialize();
         studioModule.initialize();
+        //categoryModule.initialize();
+        artPageModule.initialize();
+        pressModule.initialize();
     }
 
     return {
